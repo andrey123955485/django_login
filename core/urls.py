ec55a089a7075e01cf51606ec164fc442d570964
andrey <andreykar.adaev@gmail.com>
2024-09-23 12:58:59 +0300

    123

--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('video/', include('video.urls')),
     path('', include('login.urls')),
     path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
